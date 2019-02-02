5532a379446fd91eb45bab0f1516363423d4debf
nocturnalbeast <betrant1996@gmail.com>
2019-02-02 07:17:58 +0530

    Import grUI's dotfiles.

--- scripts/rofi-wrapper.sh
+++ scripts/rofi-wrapper.sh
@@ -19,45 +19,45 @@ Options:
 
 
 for arg in "$@"; do
-    case $arg in
-        -h|--help)
-            usage
-            exit 0
-            ;;
-        -v|--version)
-            echo -e "$NAME -- Version $VER"
-            exit 0
-            ;;
-        -r|--run)
-            rofi -modi run,drun -show drun \
-                -hide-scrollbar -show-icons -scroll-method 1
-            ;;
-        -w|--window)
-            rofi -modi window -show window \
-            	-hide-scrollbar -scroll-method 1
-            ;;
-        -c|--calculate)
-            ! hash qalc &>/dev/null && { echo "Requires 'libqalculate' installed"; exit 1; }
-            rofi -modi "calc:qalc +u8 -nocurrencies" -padding 50 \
-                -show "calc:qalc +u8 -nocurrencies" -line-padding 4 \
-                -hide-scrollbar
-            ;;
-        -b|--clipboard)
-            ! hash greenclip &>/dev/null && { echo "Requires 'greenclip' installed"; exit 1; }
-            rofi -modi "clipboard:greenclip print" -padding 50 \
-                -line-padding 4 -show "clipboard:greenclip print" \
-                -hide-scrollbar
-            ;;
-        -p|--power)
-            ANS="$(rofi -sep "|" -dmenu -i -p 'System' \
-                -width 15 \
-                -hide-scrollbar -lines 4 <<< " Lock| Logout| Reboot| Shutdown")"
-            case "$ANS" in
-                *Lock) dm-tool lock ;;
-                *Logout) session-logout || pkill -15 -t tty"$XDG_VTNR" Xorg ;;
-                *Reboot) shutdown -r now ;;
-                *Shutdown) shutdown now
-            esac ;;
-        *) echo; echo "Option does not exist: $arg"; echo; exit 2
-    esac
+	case $arg in
+		-h|--help)
+			usage
+			exit 0
+			;;
+		-v|--version)
+			echo -e "$NAME -- Version $VER"
+			exit 0
+			;;
+		-r|--run)
+			rofi -modi run,drun -show drun \
+				-hide-scrollbar -show-icons -scroll-method 1
+			;;
+		-w|--window)
+			rofi -modi window -show window \
+				-hide-scrollbar -scroll-method 1
+			;;
+		-c|--calculate)
+			! hash qalc &>/dev/null && { echo "Requires 'libqalculate' installed"; exit 1; }
+			rofi -modi "calc:qalc +u8 -nocurrencies" -lines 1\
+				-show "calc:qalc +u8 -nocurrencies" \
+				-hide-scrollbar
+			;;
+		-b|--clipboard)
+			! hash greenclip &>/dev/null && { echo "Requires 'greenclip' installed"; exit 1; }
+			rofi -modi "clipboard:greenclip print" \
+				-show "clipboard:greenclip print" \
+				-hide-scrollbar -scroll-method 1
+			;;
+		-p|--power)
+			ANS="$(rofi -sep "|" -dmenu -i -p 'System' \
+				-width 15 \
+				-hide-scrollbar -lines 4 <<< " Lock| Logout| Reboot| Shutdown")"
+			case "$ANS" in
+				*Lock) dm-tool lock ;;
+				*Logout) session-logout || pkill -15 -t tty"$XDG_VTNR" Xorg ;;
+				*Reboot) shutdown -r now ;;
+				*Shutdown) shutdown now ;;
+			esac ;;
+		*) echo; echo "Option does not exist: $arg"; echo; exit 2
+	esac
 done
