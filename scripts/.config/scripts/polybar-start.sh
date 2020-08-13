189f491e74aa8011235bd26f18774a5847131be9
nocturnalbeast <betrant1996@gmail.com>
2020-08-13 21:00:10 +0530

    Polybar: COMPLETELY rewrite the config and launch script.

--- scripts/.config/scripts/polybar-start.sh
+++ scripts/.config/scripts/polybar-start.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
+BARONE="mainbar"
+BARTWO="monbar"
+
 # terminate already running bar instances
 killall -q polybar
 
 # wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null 2>&1; do sleep 1; done
 
 # then launch bars
-polybar topbar &
-polybar bottombar &
+for m in $( polybar --list-monitors | cut -d: -f1 ); do
+    MONITOR=$m polybar $BARONE & 
+    MONITOR=$m polybar $BARTWO &
+done
+
+# get the process ID of the second bar
+BARTWO_PID=$!
+
+# wait till the second bar has launched
+while ! ls /tmp/polybar_mqueue.$BARTWO_PID >/dev/null 2>&1; do sleep 1; done
+
+# then hide the second bar, showing only the first bar
+polybar-msg -p $BARTWO_PID cmd hide >/dev/null 2>&1
