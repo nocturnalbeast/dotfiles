59d2b4cc61d17187b1e0998ceb7d8eb5859e3815
nocturnalbeast <betrant1996@gmail.com>
2025-06-29 00:36:00 +0530

    zsh: disable zsh-you-should-use till #148 is resolved.

--- zsh/.config/zsh/plughook/zsh-you-should-use.zsh
+++ zsh/.config/zsh/plughook/zsh-you-should-use.zsh
@@ -7,6 +7,7 @@ function atclone() {
 }
 
 function atinit() {
+    export YSU_HARDCORE_MODE=0
     export YSU_MESSAGE_FORMAT="$(tput setaf 3) $(tput bold)%alias_type$(tput sgr0) found for '$(tput setaf 5)$(tput bold)%command$(tput sgr0)' - '$(tput setaf 2)$(tput bold)%alias$(tput sgr0)'"
 }
 
