fd6ca1c3ec20b36ce58f431ba575caa3f5055c89
nocturnalbeast <betrant1996@gmail.com>
2025-07-10 19:12:26 +0530

    zsh: fix zsh prompt in macos.

--- zsh/.config/zsh/include/starship.zsh
+++ zsh/.config/zsh/include/starship.zsh
@@ -73,6 +73,12 @@ VIRTUAL_ENV_DISABLE_PROMPT=1
 
 setopt promptsubst
 
-PROMPT='$('/usr/bin/starship' prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-RPROMPT='$('/usr/bin/starship' prompt --right --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS"--pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-PROMPT2="$(/usr/bin/starship prompt --continuation)"
+if [ "$(uname)" == "Darwin" ] && command -v "brew" > /dev/null 2>&1; then
+    STARSHIP_BIN="/opt/homebrew/bin/starship"
+else
+    STARSHIP_BIN="/usr/bin/starship"
+fi
+
+PROMPT='$($STARSHIP_BIN prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+RPROMPT='$($STARSHIP_BIN prompt --right --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS"--pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+PROMPT2="$($STARSHIP_BIN prompt --continuation)"
